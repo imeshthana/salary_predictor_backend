64121196d887e013cf6b3bbd94f25c63ac358752
Imesh Thanapathi <imeshthanapathi@gmail.com>
2024-01-05 21:49:27 +0530

    second

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+urlpatterns += staticfiles_urlpatterns() # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
